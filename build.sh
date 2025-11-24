248baff2c3a507744efbcb76797824d3253916ec
Moisés Rodríguez Barrero <jorocan@gmail.com>
2025-11-24 23:26:50 +0100

    Evitando que se vea la variable en los logs del build

--- build.sh
+++ build.sh
@@ -6,9 +6,5 @@ mkdir -p data
 # Crear archivo YAML directamente con la variable de entorno
 echo "client_id: \"$ADSENSE_CLIENT_ID\"" > data/adsense.yaml
 
-echo "✅ data/adsense.yaml creado exitosamente"
-echo "Contenido:"
-cat data/adsense.yaml
-
 # Build con Hugo
 hugo --gc --minify
